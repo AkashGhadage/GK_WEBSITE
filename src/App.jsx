3d226406e1827e23079724ae3705719b16042a02
Akash <ghadagea2018@gmail.com>
2026-01-21 16:19:06 +0530

    dev-ph-2

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,7 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* Push content below 80px navbar */}
-      <div className="pt-[80px]">
-        <AppRoutes />
-      </div>
+      <AppRoutes />
     </>
   );
 }
